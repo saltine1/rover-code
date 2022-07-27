290036df7c48fc5b30e7bf45f25133fc8c2fd843
zzpDavid2 <51421093+zzpDavid2@users.noreply.github.com>
2022-07-27 14:43:25 -0700

    Finished Radar Test
    putting this aside until the robot drive is ready, working on gyro right now

--- Radar_Test/Radar_Test.ino
+++ Radar_Test/Radar_Test.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>;
 #include <Servo.h>;
+#include <Math.h>
 #include "Adafruit_VL53L0X.h"
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
@@ -7,14 +8,16 @@ Servo radar_servo;
 
 const int radar_servo_pin = 3;
 
-int theta = 0;
+int theta = 90;
 boolean radar_pos = true;
 
 int data[2];
 int point[2];
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  delay(500);
 
   // wait until serial port opens for native USB devices
   while (! Serial) {
@@ -30,8 +33,8 @@ void setup() {
   Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
 
   radar_servo.attach(radar_servo_pin);
-  radar_servo.write(0);
-  delay(1000);
+  radar_servo.write(theta);
+  delay(5000);
 }
 
 int lazer_measure(){
@@ -45,6 +48,7 @@ int lazer_measure(){
 //    Serial.println(measure.RangeMilliMeter);
   } else {
 //    Serial.println(" out of range ");
+      return 0;
   }
   return measure.RangeMilliMeter;
 }
@@ -61,22 +65,28 @@ void update_radar(){
 
   if(theta == 180 && radar_pos){
     radar_pos = false;
+    Serial.println("c");
+    Serial.println("c");
     }
 
   if(theta == 0 && !radar_pos){
     radar_pos = true;
+    Serial.println("c");
+    Serial.println("c");
     }
 
   int op[2];
   data[0] = theta;
   data[1] = lazer_measure();
+  Serial.print(String(point[0]) + " " + String(point[1]) + " ");
   
-  return op;
+  return;
 }
 
 int polar_to_cartesian(){
-  int x = data[1] * cos(data[0]);
-  int y = data[1] * sin(data[0]);
+  double theta = M_PI * data[0] / 180;
+  int y = data[1] * sin(theta);
+  int x = data[1] * cos(theta);
   int op[2];
   point[0] = x;
   point[1] = y;
@@ -87,15 +97,18 @@ int polar_to_cartesian(){
   void loop() {
 
   int d = lazer_measure();
-  
-  update_radar();
 
-  polar_to_cartesian();
+  for(int i=0; i<1 ;i++){
+    update_radar();
+    polar_to_cartesian();
+    Serial.println();
+    delay(100);
+  }
 
 //  Serial.println(d);
 
 //  Serial.println(String(data[0]) + " " + String(data[1]));
-  Serial.println(String(point[0]) + " " + String(point[1]));
+
 
 //  delay(2000);
 }
