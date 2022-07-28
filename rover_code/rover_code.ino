5d74e637e5cfd4796248ef9acdf8ba7e94713436
Sophia Liu <110119943+saltine1@users.noreply.github.com>
2022-07-28 10:45:22 -0700

    added motor controls + ultrasonic sensors

--- rover_code/rover_code.ino
+++ rover_code/rover_code.ino
@@ -1,11 +1,42 @@
+// sensor var setups
+int echoPin1 = 22; // front sensor
+int trigPin1 = 23;
+
+int echoPin2 = 24; // left sensor
+int trigPin2 = 25;
+
+int echoPin3 = 26; // right sensor
+int trigPin3 = 27;
+
+int echoPin4 = 28; // arm sensor
+int trigPin4 = 29;
+
+long duration1, duration2, duration3, duration4, in1, in2, in3, in4;
+
+// motor var setups
+int frontIn1 = 2; // In 1 and 2 are for the left motors, while In 3 and 4 are for the right motors
+int frontIn2 = 3;
+int frontIn3 = 5;
+int frontIn4 = 6;
+
+int backIn1 = 50;
+int backIn2 = 51;
+int backIn3 = 52;
+int backIn4 = 53;
+
+int frontENA = 4;
+int frontENB = 7;
+int backENA = 47;
+int backENB = 48;
+
 void setup() {
-  Serial.begin (10000);
+  Serial.begin(9600);
   // sensor pin setups
-  pinMode(8, INPUT);  //echo front 
-  pinMode(9, OUTPUT); //trig front
+  pinMode(echoPin1, INPUT);  //echo front 
+  pinMode(trigPin1, OUTPUT); //trig front
   
-  pinMode(4, INPUT); // echo left
-  pinMode(11, OUTPUT); // trig left
+  pinMode(echoPin2, INPUT); // echo left
+  pinMode(echoPin2, OUTPUT); // trig left
   
   pinMode(2, INPUT); // echo right
   pinMode(5, OUTPUT); // trig right
@@ -19,43 +50,19 @@ void setup() {
   pinMode(24, OUTPUT); // front right
   pinMode(25, OUTPUT); // front right
   
-  pinMode(50, OUTPUT); // back left
-  pinMode(51, OUTPUT); // back left
-  pinMode(52, OUTPUT); // back right
-  pinMode(53, OUTPUT); // back right
+  pinMode(2, OUTPUT); // back left
+  pinMode(3, OUTPUT); // back left
+  pinMode(5, OUTPUT); // back right
+  pinMode(6, OUTPUT); // back right
 
   pinMode(26, OUTPUT); // front left speed
   pinMode(27, OUTPUT); // front right speed
 
-  pinMode(47, OUTPUT); // back left speed
-  pinMode(48, OUTPUT); // back right speed
+  pinMode(4, OUTPUT); // back left speed
+  pinMode(7, OUTPUT); // back right speed
 }
 
-// sensor var setups
-  int echoPin1 = 8; // front sensor
-  int trigPin1 = 9;
-
-  int echoPin2 = 4; // left sensor
-  int trigPin2 = 11;
-
-  int echoPin3 = 2; // right sensor
-  int trigPin3 = 5;
-
-  int echoPin4 = 12; // arm sensor
-  int trigPin4 = 10;
-
-  long duration1, duration2, duration3, duration4, in1, in2, in3, in4;
   
-  // motor var setups
-  int frontIn1 = 22; // In 1 and 2 are for the left motors, while In 3 and 4 are for the right motors
-  int frontIn2 = 23;
-  int frontIn3 = 24;
-  int frontIn4 = 25;
-
-  int backIn1 = 50;
-  int backIn2 = 51;
-  int backIn3 = 52;
-  int backIn4 = 53;
 
 void trigOnOff(int trigPinNum, int trigPinNumVar) {
   digitalWrite(trigPinNum, LOW); // trig off
@@ -64,31 +71,96 @@ void trigOnOff(int trigPinNum, int trigPinNumVar) {
   delayMicroseconds(10);
   digitalWrite(trigPinNumVar, LOW);
 }
+
+void Move(int left, int right){
+  if (left > 0){
+    digitalWrite(frontIn1, HIGH);
+    digitalWrite(frontIn2, LOW);
+    digitalWrite(backIn1, HIGH);
+    digitalWrite(frontIn2, LOW);
+  }
+  else{
+    digitalWrite(frontIn1, LOW);
+    digitalWrite(frontIn2, HIGH);
+    digitalWrite(backIn1, LOW);
+    digitalWrite(backIn2, HIGH);
+    
+  }
+    if (right > 0){
+    digitalWrite(frontIn3, HIGH);
+    digitalWrite(frontIn4, LOW);
+    digitalWrite(backIn3, HIGH);
+    digitalWrite(frontIn4, LOW);
+  }
+  else{
+    digitalWrite(frontIn3, LOW);
+    digitalWrite(frontIn4, HIGH);
+    digitalWrite(backIn3, LOW);
+    digitalWrite(backIn4, HIGH);
+  }
+  analogWrite(frontENA, int(abs(double(left))/1000*255));
+  analogWrite(backENA, int(abs(double(left))/1000*255));
+  analogWrite(frontENB, int(abs(double(right))/1000*255));
+  analogWrite(backENB, int(abs(double(right))/1000*255));
+}
+
+int ports[] = {22, 24, 26, 28};
+
+double ultrasonicDistance(int N){
+  int port = ports[N];
+  digitalWrite(23, LOW); // trig off
+  delayMicroseconds(5);
+  digitalWrite(23, HIGH); // trig on
+  delayMicroseconds(10);
+  digitalWrite(23, LOW);
+  int duration = pulseIn(port, HIGH);
+  pinMode(port, INPUT);
+  return double((duration/2)/74);
   
+}
 
 void loop() {
   // Part 1: Obstacle Course
   // First, measure the distance from obstacles using ultrasonic sensors as it goes forwards
-  trigOnOff(9, trigPin1); // front
-  trigOnOff(11, trigPin2); // left
-  trigOnOff(5, trigPin3); // right
-  trigOnOff(10, trigPin4); // arm
+  trigOnOff(23, trigPin1); // front
+  trigOnOff(25, trigPin2); // left
+  trigOnOff(27, trigPin3); // right
+  trigOnOff(29, trigPin4); // arm
+//  
+//  pinMode(8, INPUT); //recieve signal echo
+//  duration1 = pulseIn(8, HIGH); //front sensor
+//  pinMode(4, INPUT);
+//  duration2 = pulseIn(4, HIGH); // left sensor
+//  pinMode(2, INPUT);
+//  duration3 = pulseIn(2, HIGH); // right sensor
+//  pinMode(5, INPUT);
+//  duration4= pulseIn(5, HIGH); // arm sensor
+//
+//  //conversions
+//  in1 = (duration1/2)/74; // front
+//  in2 = (duration2/2)/74; // left
+//  in3 = (duration3/2)/74; // right
+//  in4 = (duration4/2)/74; // arm
+//  analogWrite(26, 100); // front ENA pin
+//  analogWrite(27, 100); // front ENB pin
+//  analogWrite(47, 100); // back ENA pin
+//  analogWrite(48, 100); // back ENB pin
+
+  Serial.print(String(ultrasonicDistance(0)) + " ");
+  Serial.print(String(ultrasonicDistance(1)) + " ");
+  Serial.print(String(ultrasonicDistance(2)) + " ");
+  Serial.println(String(ultrasonicDistance(3)) + " ");
+  delay(500);
   
-  pinMode(8, INPUT); //recieve signal echo
-  duration1 = pulseIn(8, HIGH); //front sensor
-  pinMode(4, INPUT);
-  duration2 = pulseIn(4, HIGH); // left sensor
-  pinMode(2, INPUT);
-  duration3 = pulseIn(2, HIGH); // right sensor
-  pinMode(5, INPUT);
-  duration4= pulseIn(5, HIGH); // arm sensor
-
-  //conversions
-  in1 = (duration1/2)/74; // front
-  in2 = (duration2/2)/74; // left
-  in3 = (duration3/2)/74; // right
-  in4 = (duration4/2)/74; // arm
-
+//  Move(500, 500);
+//  delay(2000);
+//  Move(-500, 500);
+//  delay(2000);
+//  Move(500, -500);
+//  delay(2000);
+//  Move(-500, -500);
+//  delay(2000);
+//  
   // WIP BELOW
   
   // Use if/else statements to determine if there is an obstacle
@@ -99,3 +171,4 @@ void loop() {
   
 
 }
+
